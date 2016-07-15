12312b17f565747551aebcf819deb600fe937f1f
Jessica <jessica.leivers@bbc.co.uk>
2016-07-15 13:09:25 +0100

    adds bower and gulp to project

---
+++ README.md
@@ -0,0 +1,5 @@
+### Installation
+`npm install`
+
+### Usage
+`npm start`
--- app.js
+++ app.js
@@ -4,14 +4,21 @@ var app = express();
 
 var port = 5000;
 
+// MIDDLEWARE
+app.use(express.static('public'));
+app.use(express.static('src/views'));
+
+
+// SERVE
 app.listen(port, function(err){
     console.log('server on port: ' + port);
 });
 
 //ROUTING
 app.get('/', function(req, res){
-
+    res.serve('src/views/index.html');
 });
-app.post();
-app.put();
-app.delete();
+
+// app.post();
+// app.put();
+// app.delete();
