fe5ae036a246fd480f3922a10be01db251507ac4
Jessica <jessica.leivers@bbc.co.uk>
2016-07-24 23:41:44 +0100

    adds handlebars templating functionality

--- app.js
+++ app.js
@@ -2,27 +2,31 @@ var express = require('express');
 
 var app = express();
 
-var port = 5000;
-
 // MIDDLEWARE
 app.use(express.static('public'));
-app.use(express.static('src/views'));
+// app.use(express.static('src/views'));
 app.set('views', './src/views');
 
+
+// HANDLEBARS
 var handlebars = require('express-handlebars');
+app.engine('.hbs', handlebars({extname: '.hbs'}));
 app.set('view engine', '.hbs');
 
 
 // SERVE
+var port = 5000;
 app.listen(port, function(err){
     console.log('server on port: ' + port);
 });
 
 //ROUTING
 app.get('/', function(req, res){
-    res.serve('src/views/index.html');
+    res.render('index');
 });
-
+app.get('/#about', function(req, res){
+    res.render('index');
+});
 // app.post();
 // app.put();
 // app.delete();
