8219dcc4436a522d3f1a873fe84888274ceec301
Jessica Leivers <sarahjessicaleivers@gmail.com>
2016-07-26 08:26:43 +0100

    scss gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,15 @@
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var jscs = require('gulp-jscs');
+var scss = require('gulp-sass');
 
 var jsFiles = ['*.js', 'src/**/*.js'];
+var scssFiles = ['*.scss', 'src/**/*.scss'];
 
+
+// CONSISTENT CODING STYLE
 gulp.task('style', function(){
-    gulp.src(jsFiles)
+  gulp.src(jsFiles)
     .pipe(jshint())
     .pipe(jshint.reporter('jshint-stylish', {
         verbose : true
@@ -13,5 +17,11 @@ gulp.task('style', function(){
     .pipe(jscs());
 });
 
+// CSS
+gulp.task('scss', function(){
+  gulp.src(scssFiles)
+    .pipe(scss().on('error', scss.logError))
+    .pipe(gulp.dest('./public/'));
+});
 
 gulp.task('default', function(){});
