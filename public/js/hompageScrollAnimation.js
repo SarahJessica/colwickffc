45575ddd3049eb14b574db282e617eed8ac2f2be
Jessica Leivers <sarahjessicaleivers@gmail.com>
2017-01-09 16:08:16 +0000

    fixes jumpiness in Chrome

--- public/js/hompageScrollAnimation.js
+++ public/js/hompageScrollAnimation.js
@@ -2,11 +2,15 @@
 
 (function iife() {
   const splashPxPerVh = Math.floor($("#splash").height() / 100);
+  var imgArray = [];
+
+  preloadImages(imgArray);
+  console.log(imgArray);
 
   $(window).on('scroll', function() {
     var topOffset = $(this).scrollTop();
-    var bgImg = currentBgImg(percentageDown(topOffset));
-    console.log(bgImg);
+    var bgImg = currentBgImg(percentageDown(topOffset), imgArray);
+
     $("#splash").css("background-image", "url(" + bgImg + ")");
     if (topOffset > $("#splash").height() ) {
       $("#splash").css("display", "none");
@@ -17,9 +21,19 @@
 
 }());
 
-function currentBgImg(percent){
+function preloadImages(imgs) {
+  // remember images are labelled 1 to 100 not 0 to 99
+  for (var i = 0; i <= 99; i++) {
+    imgs[i] = new Image();
+    imgs[i].src = "/img/100-wave-frames/" + (i + 1).toString() + ".jpg";
+  }
+  return imgs;
+}
+
+function currentBgImg(percent, imgs){
+
   if(percent < 100  && percent > 0 ) {
-      return "/img/100-wave-frames/" + percent.toString() + ".jpg";
+      return imgs[percent].src;
   } else if (percent >= 100 ){
     return "/img/100-wave-frames/100.jpg";
   } else {
